852c04e6b936812bffa35f1345992d2c9fd53fd0
Shubhamjindal0801 <52204183+Shubhamjindal0801@users.noreply.github.com>
2023-04-12 13:35:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
-//your JS code here. If required.
+
+        var btn1 = document.getElementById("applause").addEventListener("click", ()=>{
+            const audio1 = new Audio("https://cdn.pixabay.com/download/audio/2021/08/09/audio_ff81343224.mp3?filename=short-crowd-cheer-6713.mp3")
+            audio1.play();
+        });
+        var btn2 = document.getElementById("boo").addEventListener("click", ()=>{
+            const audio1 = new Audio("https://cdn.pixabay.com/download/audio/2021/08/09/audio_0b8c6f0c5b.mp3?filename=boo-and-laugh-7060.mp3")
+            audio1.play();
+        });
+        var btn3 = document.getElementById("gasp").addEventListener("click", ()=>{
+            const audio1 = new Audio("https://cdn.pixabay.com/download/audio/2021/08/04/audio_209174bd86.mp3?filename=gasp-6253.mp3")
+            audio1.play();
+        });
+        var btn4 = document.getElementById("tada").addEventListener("click", ()=>{
+            const audio1 = new Audio("https://cdn.pixabay.com/download/audio/2021/08/04/audio_473a42432c.mp3?filename=tada-fanfare-a-6313.mp3")
+            audio1.play();
+        });
+        var btn5 = document.getElementById("victory").addEventListener("click", ()=>{
+            const audio1 = new Audio("https://cdn.pixabay.com/download/audio/2021/08/09/audio_e0ee349421.mp3?filename=car-passing-by-6899.mp3")
+            audio1.play();
+        });
+        var btn6 = document.getElementById("wrong").addEventListener("click", ()=>{
+            const audio1 = new Audio("https://cdn.pixabay.com/download/audio/2022/12/13/audio_efee639af6.mp3?filename=wrong-place-129242.mp3")
+            audio1.play();
+        });
+    
